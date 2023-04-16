81815c49a65e956e58a5d289b34b8501c5348363
0v0d <w>
2023-04-16 19:33:26 +0900

    Common processing and message definition

--- cog/Calculation.py
+++ cog/Calculation.py
@@ -5,58 +5,59 @@ class Calculator(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    ARGUMENTS_ERROR_MESSAGE = "引数が少なすぎます。2つ以上の数値を入力してください。"
+    NUMERIC_ERROR_MESSAGE = "数値以外の入力がありました。数値を入力してください。"
+    ZERO_DIVISION_ERROR_MESSAGE = "0で割ることはできません。別の数値を入力してください。"
+
+    async def check_args(self,ctx, args):
+        numbers = args.split(",")
+        if not len(numbers) >= 2:
+            await ctx.send(self.ARGUMENTS_ERROR_MESSAGE+f"{ctx.command.name}")
+            return None
+        try:
+            numbers = list(map(float, numbers))
+        except ValueError:
+            await ctx.send(self.NUMERIC_ERROR_MESSAGE)
+            return None
+        return numbers
+
     @commands.command(name='add')
     async def add_command(self, ctx, args):
-        numbers = args.split(",")
-        if len(numbers) < 2:
-            await ctx.send("引数が少なすぎます。2つ以上の数値を入力してください。add")
+        numbers = await self.check_args(ctx, args)
+        if numbers is None:
             return
-        try:
-            result = sum(map(float, numbers))
-            await ctx.send(f"結果は{result}")
-        except ValueError:
-            await ctx.send("数値以外の入力がありました。数値を入力してください。")
+        result = sum(numbers)
+        await ctx.send(f"結果は{result}")
 
     @commands.command(name='sub')
     async def sub_command(self, ctx, args):
-        numbers = args.split(",")
-        if len(numbers) < 2:
-            await ctx.send("引数が少なすぎます。2つ以上の数値を入力してください。sub")
+        numbers = await self.check_args(ctx, args)
+        if numbers is None:
             return
-        try:
-            result = float(numbers[0])
-            for num in numbers[1:]:
-                result -= float(num)
-            await ctx.send(f"結果は{result}")
-        except ValueError:
-            await ctx.send("数値以外の入力がありました。数値を入力してください。")
+        result = numbers[0]
+        for num in numbers[1:]:
+            result -= num
+        await ctx.send(f"結果は{result}")
 
     @commands.command(name='mul')
     async def mul_command(self, ctx, args):
-        numbers = args.split(",")
-        if len(numbers) < 2:
-            await ctx.send("引数が少なすぎます。2つ以上の数値を入力してください。mul")
+        numbers = await self.check_args(ctx, args)
+        if numbers is None:
             return
-        try:
-            result = 1
-            for num in numbers:
-                result *= float(num)
-            await ctx.send(f"結果は{result}")
-        except ValueError:
-            await ctx.send("数値以外の入力がありました。数値を入力してください。")
+        result = 1
+        for num in numbers:
+            result *= num
+        await ctx.send(f"結果は{result}")
 
     @commands.command(name='div')
     async def div_command(self, ctx, args):
-        numbers = args.split(",")
-        if len(numbers) < 2:
-            await ctx.send("引数が少なすぎます。2つ以上の数値を入力してください。div")
+        numbers = await self.check_args(ctx, args)
+        if numbers is None:
             return
-        try:
-            result = float(numbers[0])
-            for num in numbers[1:]:
-                result /= float(num)
-            await ctx.send(f"結果は{result}")
-        except ValueError:
-            await ctx.send("数値以外の入力がありました。数値を入力してください。")
-        except ZeroDivisionError:
-            await ctx.send("0で割ることはできません。別の数値を入力してください。")
+        if 0 in numbers[1:]:
+            await ctx.send(self.ZERO_DIVISION_ERROR_MESSAGE)
+            return
+        result = numbers[0]
+        for num in numbers[1:]:
+            result /= num
+        await ctx.send(f"結果は{result}")
