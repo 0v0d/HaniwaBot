0c7f4e9cf76d454f1ce0db4474aa0a281efb4595
0v0d <86046148+0v0d@users.noreply.github.com>
2023-05-08 10:33:45 +0900

    add keep_alive.py and merged

--- main.py
+++ main.py
@@ -2,31 +2,38 @@ import discord
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+from keep_alive import keep_alive
 
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 from cog.Calculation import Calculator
 from cog.sub import MyCog
 from cog.Random import Random
 
-load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 client = commands.Bot(
-    command_prefix=commands.when_mentioned_or("h"),
-    intents=intents,
-    case_insensitive=True,
-    help_command=None,
+  command_prefix=commands.when_mentioned_or("h"),
+  intents=intents,
+  case_insensitive=True,
+  help_command=None,
 )
 
 
 @client.event
 async def on_ready():
-    print('ログインしました')
-    await client.add_cog(MyCog(client))
-    await client.add_cog(Calculator(client))
-    await client.add_cog(Random(client))
-    print(client.user.name)  # Botの名前
-    print(client.user.id)  # ID
-    await client.change_presence(activity=discord.Game(name=f"{len(client.guilds)}サーバー"))
+  print('ログインしました')
+  await client.add_cog(MyCog(client))
+  await client.add_cog(Calculator(client))
+  await client.add_cog(Random(client))
+  print(client.user.name)  # Botの名前
+  print(client.user.id)  # ID
+  await client.change_presence(activity=discord.Game(
+    name=f"{len(client.guilds)}サーバー"))
 
 
-client.run(os.getenv("DISCORD_BOT_TOKEN"))
+keep_alive()
+try:
+  client.run(os.environ.get("DISCORD_BOT_TOKEN"))
+except:
+  os.system("kill 1")
